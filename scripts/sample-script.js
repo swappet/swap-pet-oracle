5347c5ff59739b041b35fc40a5941d301295ffef
dapp8coder <dapp.coder@protonmail.com>
2020-10-30 09:44:09 +0800

    sync

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,7 +1,7 @@
 // We require the Buidler Runtime Environment explicitly here. This is optional 
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
-// When running the script with `hardhat run <script>` you'll find the Buidler
+// When running the script with `buidler run <script>` you'll find the Buidler
 // Runtime Environment's members available in the global scope.
 const bre = require("@nomiclabs/buidler");
 
