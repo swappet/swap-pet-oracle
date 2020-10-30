1e495d9d40a5fc7186eb08beafbfd88d351a5a28
dapp8coder <dapp.coder@protonmail.com>
2020-10-30 12:35:52 +0800

    sync

--- test/SwapPetOracleTest.js
+++ test/SwapPetOracleTest.js
@@ -4,6 +4,7 @@ const SwapPetOracle = artifacts.require('SwapPetOracle')
 const { BN, expectRevert } = require('@openzeppelin/test-helpers')
 const assert = require('assert');
 
+// Traditional Truffle test
 contract('SwapPetOracle', (accounts) => {
   const [deployer, user1, user2, user3] = accounts
   let spOracle
@@ -25,3 +26,19 @@ contract('SwapPetOracle', (accounts) => {
     })   
   })
 }) 
+// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
+describe("SwapPetOracle contract", function() {
+  let accounts,deployer, user1, user2, user3;
+
+  before(async function() {
+    accounts = await web3.eth.getAccounts();
+    [deployer, user1, user2, user3] = accounts 
+  });
+
+  describe("Deployment", function() {
+    it("Should deploy with the right SwapPetOracle", async function() {
+      const spOracle = await SwapPetOracle.new(user1,user2,{ from: deployer });
+      expect(await spOracle.swapper()).to.equal(user2) ;
+    });
+  });
+});
