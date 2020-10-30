0f250cafd0c914d68a1e444c18f88badd1dfa052
dapp8coder <dapp.coder@protonmail.com>
2020-10-30 08:47:42 +0800

    edit hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,24 +4,34 @@ require('@nomiclabs/hardhat-truffle5');
 require('solidity-coverage'); 
 require('buidler-gas-reporter'); 
 
+const fs = require('fs');
+const path = require('path'); 
+const infuraKey = fs.readFileSync(path.resolve(__dirname, '.infuraKey')).toString().trim(); 
 // You have to export an object to set up your config
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more 
 module.exports = {
-  defaultNetwork: 'buidlerevm',
+  defaultNetwork: 'hardhat',  
   networks: {
+    hardhat: {},
     buidlerevm: {},
     coverage: {
       url: 'http://127.0.0.1:8555',
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey1, privateKey2, ...]
+    },
   },
   solc: {
     version: '0.7.0',
-    optimizer: {
-      enabled: true,
-      runs: 20000,
-    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20000
+      }
+    } 
   },
   gasReporter: {
     enabled: true,
