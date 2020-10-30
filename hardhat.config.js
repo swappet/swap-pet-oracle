5836b056b5a5c1919a714a78a462c9636aed545b
dapp8coder <dapp.coder@protonmail.com>
2020-10-30 09:19:54 +0800

    sync

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
       gasPrice:8000000000,        // default:auto
       gasMultiplier:1,            // default:1
       HardhatNetworkHDAccountsConfig:{
-        mnemonic:"test test test test test test test test test test test junk",
+        mnemonic:"test test test test test test test test test test test junkd",
         // mnemonic:`${mnemonic}`,
         initialIndex:0,
         path:"m/44'/60'/0'/0",
